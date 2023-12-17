d2ca8d373dc4ba16bd537d1dcb01ab07cf5d1b53
sebAlza <sebastien@studio-manette.com>
2023-12-17 23:34:39 +0100

    clean + loading scene

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public float speed;
+    private float x;
+    public float DestinationPoint;
+    public float OriginalPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        //OriginalPoint = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = transform.position.x;
+        x += speed * Time.deltaTime;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+        if (x <= DestinationPoint)
+        {
+            x = OriginalPoint;
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+    }
+}
